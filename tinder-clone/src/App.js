587b35c440dd52c885f4340ed15107a4ef1a518c
pitou <akashdevkate@outlook.com>
2020-11-16 20:20:01 +0530

    na

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    // BEM class naing convention
+    <div className="app">
+
+      <Header/>
+      {/* Tinde
+      rcards */}
+      {/* Swipebuttons */}
+
+    </div>
+  );
+}
+
+export default App;
