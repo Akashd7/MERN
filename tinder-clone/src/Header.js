88b19188de9f53c286dc502bb9cc5edc4e4f6f7c
pitou <akashdevkate@outlook.com>
2020-11-17 13:19:52 +0530

    na

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.css';
+import PersonIcon from '@material-ui/icons/Person';
 
 function Header() {
     return (
         <div className='header'>
             <h2>i am a header</h2>
+            <PersonIcon fontSize="large" className="header_icon" />
         </div>
     )
 }
