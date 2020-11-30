1ecd87ac3a3c2a430e6c7d1c01272d8b61d9aafd
pitou <akashdevkate@outlook.com>
2020-11-30 18:24:30 +0530

    1

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
 import IconButton from "@material-ui/core/IconButton";
+import ForumIcon from '@material-ui/icons/Forum';
+
 
 function Header() {
     return (
@@ -14,6 +16,9 @@ function Header() {
                 src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo-500x281.png"
                 alt=""
             />
+        <IconButton>
+            <ForumIcon fontSize='large' className="header_icon"/>
+        </IconButton>
         </div>
     )
 }
